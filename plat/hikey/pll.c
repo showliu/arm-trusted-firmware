8f34872edd126ae296d0fe557bb403143012eeaf
Jerome Forissier <jerome.forissier@linaro.org>
2015-03-20 18:40:07 +0100

    HiKey: BL1: do not write at address 0x0
    Avoid an unwanted GCC 4.9 optimization by testing at adress 0x4 instead of
    0x0.
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

--- plat/hikey/pll.c
+++ plat/hikey/pll.c
@@ -1372,8 +1372,8 @@ void hi6220_pll_init(void)
 	init_ddr();
 	init_ddrc_qos();
 
-	mmio_write_32(0x0, 0xa5a55a5a);
-	INFO("ddr test value:0x%x\n", mmio_read_32(0x0));
+	mmio_write_32(0x4, 0xa5a55a5a);
+	INFO("ddr test value:0x%x\n", mmio_read_32(0x4));
 
 	init_mmc_pll();
 	reset_mmc0_clk();
